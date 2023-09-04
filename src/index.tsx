98c49e34efd89d89f6171931120e94b5a54839f6
iturres <emanueliturres.co@gmail.com>
2023-09-04 01:50:22 -0300

    feat: on 'index.tsx' setup the following:
    - Created the root element.
    - Setup browser router.
    
    -Imported Bootstrap and custom Styles.

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,19 @@
-// ? index.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.scss';
+import App from './App.tsx';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
