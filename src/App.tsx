d74d8c8cdb3c9db84e43cf88634f1c2294e15bb1
iturres <emanueliturres.co@gmail.com>
2023-09-04 01:48:54 -0300

    feat: on 'App.tsx' component:
    - setup routes to 'AccessPage' component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,14 @@
-// ? App.tsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import AccessPage from './components/pages/AccessPage.tsx';
+
+const App = () => (
+  <>
+    <Routes>
+      <Route path="/" element={<AccessPage />} />
+    </Routes>
+  </>
+);
+
+export default App;
