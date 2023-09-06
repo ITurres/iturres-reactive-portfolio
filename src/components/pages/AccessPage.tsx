8ca7ef346ef23b4e1ee1da7c806e60c594f6fd81
iturres <emanueliturres.co@gmail.com>
2023-09-05 23:24:16 -0300

    Add new component 'AccessPage.tsx'

--- src/components/pages/AccessPage.tsx
+++ src/components/pages/AccessPage.tsx
@@ -1,18 +1,46 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import '../../styles/pages/AccessPage.scss';
 import '../../styles/animations/button-slide.scss';
+import '../../styles/animations/shrink-and-center.scss';
+import AccessPageVideo from '../animations/AccessPageVideo.tsx';
 
-const AccessPage = () => (
-  <main>
-    <span>Arthur Iturres</span>
-    <h1>Delivering The Future</h1>
-    <Link to="/homepage">
-      <button type="button" className="my-btn color2 slide-up">
-        Access
-      </button>
-    </Link>
-  </main>
-);
+const AccessPage = () => {
+  const navigate = useNavigate();
+
+  const handleAccess = () => {
+    const $accessPageMain = document.querySelector<HTMLDivElement>('#access-page-main');
+    if ($accessPageMain) {
+      $accessPageMain.classList.add('shrink-and-center');
+    }
+
+    const $videoElement = document.querySelector<HTMLVideoElement>('#access-page-video');
+    if ($videoElement) {
+      setTimeout(() => {
+        $videoElement.style.display = 'block';
+        $videoElement.play();
+      }, 200);
+
+      $videoElement.addEventListener('ended', () => navigate('/homepage'));
+    }
+  };
+
+  return (
+    <>
+      <main id="access-page-main">
+        <span>Arthur Iturres</span>
+        <h1>Delivering The Future</h1>
+        <button
+          type="button"
+          className="my-btn color2 slide-up"
+          onClick={handleAccess}
+        >
+          Access
+        </button>
+      </main>
+      <AccessPageVideo />
+    </>
+  );
+};
 
 export default AccessPage;
