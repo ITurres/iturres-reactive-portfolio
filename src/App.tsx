8e3164aa5def4352947727c4ec0f08929e670c31
iturres <emanueliturres.co@gmail.com>
2023-09-06 23:52:17 -0300

    feat: on 'App.tsx' Added route to 'HomePage' component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import AccessPage from './components/pages/AccessPage.tsx';
+import HomePage from './components/pages/HomePage.tsx';
 
 const App = () => (
   <>
     <Routes>
       <Route path="/" element={<AccessPage />} />
+      <Route path="homepage" element={<HomePage />} />
     </Routes>
   </>
 );
