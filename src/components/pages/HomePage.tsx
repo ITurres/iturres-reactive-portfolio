8e53df3de6cdd1c05594df5495ea7396feb3dc38
iturres <emanueliturres.co@gmail.com>
2023-09-06 23:51:43 -0300

    Add 'HomePage.tsx' new page component

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect } from 'react';
 
+import profileImage from '../../assets/images/profile/profile.png';
+import homepageDotsImage from '../../assets/images/bg/homepage-dots-img.png';
+import '../../styles/pages/HomePage.scss';
+
 const HomePage = () => {
   useEffect(() => {
     const $homepage = document.querySelector<HTMLDivElement>('#homepage');
@@ -9,9 +13,32 @@ const HomePage = () => {
   });
 
   return (
-    <div id="homepage" className="homepage">
-      <h1>Home Page</h1>
-    </div>
+    <section id="homepage" className="homepage container">
+      <div className="homepage__about">
+        <h1 className="homepage__about-title hue-rotate">
+          i&apos;m arthur! your dev &#128075;
+        </h1>
+        <p className="homepage__about-p">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse,
+          suscipit et? Quo facere fugiat deserunt expedita pariatur consequuntur
+          quos possimus quidem repellat ipsam sequi, mollitia perspiciatis
+          suscipit id ipsa voluptates.
+        </p>
+      </div>
+      <div className="homepage__images">
+        <img
+          src={homepageDotsImage}
+          alt="an abstract dotted figure"
+          className="image-layer figure rotate-resize"
+        />
+        <img
+          src={profileImage}
+          alt="my profile"
+          width="50%"
+          className="image-layer profile"
+        />
+      </div>
+    </section>
   );
 };
 
