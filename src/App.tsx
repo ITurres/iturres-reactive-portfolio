828a441c251f936b894256e6d94fbfdc8050ae75
iturres <emanueliturres.co@gmail.com>
2023-09-08 23:59:19 -0300

    feat: in 'App.tsx': Import and render 'LineCount' component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 
 import AccessPage from './components/pages/AccessPage.tsx';
 import HomePage from './components/pages/HomePage.tsx';
+import LineCount from './components/LineCount.tsx';
 
 const App = () => (
   <>
+    <LineCount />
     <Routes>
       <Route path="/" element={<AccessPage />} />
       <Route path="homepage" element={<HomePage />} />
