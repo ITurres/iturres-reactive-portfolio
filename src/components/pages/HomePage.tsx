70b04a18a44126ad22e0e05b3a68548368e2e3d9
iturres <emanueliturres.co@gmail.com>
2023-09-12 02:20:55 -0300

    Feat: on 'HomePage.tsx' Import and Render 'Navbar' component

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -3,10 +3,12 @@ import { Routes, Route, Outlet } from 'react-router-dom';
 
 import AboutPage from './AboutPage.tsx';
 import LineCount from '../UI/LineCount.tsx';
+import Navbar from '../UI/Navbar.tsx';
 
 const HomePage: React.FC = () => (
   <>
     <LineCount />
+    <Navbar />
     <Routes>
       <Route path="/" element={<Outlet />}>
         <Route path="/" element={<AboutPage />} />
