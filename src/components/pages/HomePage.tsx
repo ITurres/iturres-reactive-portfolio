728126f4f314ffe565a76b3c1aefa9d442e3aea7
iturres <emanueliturres.co@gmail.com>
2023-09-14 23:09:55 -0300

    feat: Add route to 'ExpertiseSummaryPage' on 'HomePage' page component

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -4,6 +4,7 @@ import { Routes, Route, Outlet } from 'react-router-dom';
 import AboutPage from './AboutPage.tsx';
 import LineCount from '../UI/LineCount.tsx';
 import Navbar from '../UI/Navbar.tsx';
+import ExpertiseSummaryPage from './ExpertiseSummaryPage.tsx';
 
 const HomePage: React.FC = () => (
   <>
@@ -12,6 +13,7 @@ const HomePage: React.FC = () => (
     <Routes>
       <Route path="/" element={<Outlet />}>
         <Route path="/" element={<AboutPage />} />
+        <Route path="/expertise" element={<ExpertiseSummaryPage />} />
       </Route>
     </Routes>
   </>
