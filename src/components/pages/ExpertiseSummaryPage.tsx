c2fa5cd164b27c5a6ec5ac8022009b039398fe6a
iturres <emanueliturres.co@gmail.com>
2023-09-16 02:00:45 -0300

    Fix: animation affecting 'LineCount' and 'Navbar' on 'ExpertiseSummaryPage'
    - Split 'bg--container' and 'blend-in-out' divs into separate divs.
    
    Now the animation 'blend-in-out' only affects its div and its children.
    Leaving 'LineCount' and 'Navbar' components unaffected.

--- src/components/pages/ExpertiseSummaryPage.tsx
+++ src/components/pages/ExpertiseSummaryPage.tsx
@@ -5,27 +5,29 @@ import expertiseData from '../../db/expertiseSummary.json';
 import '../../styles/pages/ExpertiseSummaryPage.scss';
 
 const ExpertiseSummaryPage: React.FC = () => (
-  <div className="bg--container blend-in-out">
-    <main className="expertise container ">
-      {Object.entries(expertiseData).map((expertiseSummary) => {
-        const [expertiseCategory, expertiseItems] = expertiseSummary;
-        return (
-          <section key={expertiseCategory} className="expertise__card">
-            <h2 className="text-hue-rotate">{expertiseCategory}</h2>
-            <div className="expertise__items">
-              {expertiseItems.map((item) => (
-                <span key={item}>{item}</span>
-              ))}
-            </div>
-            <div className="squares--container">
-              <div className="square-item" />
-              <div className="square-item" />
-              <div className="square-item" />
-            </div>
-          </section>
-        );
-      })}
-    </main>
+  <div className="bg--container">
+    <div className="blend-in-out">
+      <main className="expertise container ">
+        {Object.entries(expertiseData).map((expertiseSummary) => {
+          const [expertiseCategory, expertiseItems] = expertiseSummary;
+          return (
+            <section key={expertiseCategory} className="expertise__card">
+              <h2 className="text-hue-rotate">{expertiseCategory}</h2>
+              <div className="expertise__items">
+                {expertiseItems.map((item) => (
+                  <span key={item}>{item}</span>
+                ))}
+              </div>
+              <div className="squares--container">
+                <div className="square-item" />
+                <div className="square-item" />
+                <div className="square-item" />
+              </div>
+            </section>
+          );
+        })}
+      </main>
+    </div>
   </div>
 );
 
