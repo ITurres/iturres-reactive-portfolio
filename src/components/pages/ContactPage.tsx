b2e38f1313546b854385684e1934ad51402196ec
iturres <emanueliturres.co@gmail.com>
2023-09-19 22:09:30 -0300

    Add new component 'ContactPage' base-code with 'Drone' component

---
+++ src/components/pages/ContactPage.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+// import Drone from '../animations/Drone.tsx';
+
+const ContactPage: React.FC = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const laptopWidth = 1366;
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [windowWidth]);
+
+//   return <div>{windowWidth >= laptopWidth ? <Drone /> : null}</div>;
+};
+
+export default ContactPage;
