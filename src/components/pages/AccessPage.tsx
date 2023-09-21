aac9e8ee478598144baaa594338d6f71f462dcf3
iturres <emanueliturres.co@gmail.com>
2023-09-20 21:01:00 -0300

    Feat: Enhance 'AccessPage' component with new animation
    - Import 'FloatingAstronaut' component.
    - Declare and Initialize 'AstronautStyleProps' props.
    - Render 'FloatingAstronaut' component and pass props.

--- src/components/pages/AccessPage.tsx
+++ src/components/pages/AccessPage.tsx
@@ -1,9 +1,28 @@
 import React, { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 import '../../styles/pages/AccessPage.scss';
 import '../../styles/animations/button-slide.scss';
 import '../../styles/animations/shrink-and-center.scss';
+
 import AccessPageVideo from '../animations/AccessPageVideo.tsx';
+import FloatingAstronaut from '../animations/FloatingAstronaut.tsx';
+
+const AstronautStyleProps = {
+  animationSpeed: 30,
+  maxWidth: 150,
+  move: {
+    fromTop: 0,
+    toTop: 300,
+    fromLeft: -100,
+    toLeft: 100,
+    fromRight: 0,
+    toRight: 0,
+  },
+  rotation: {
+    rotate: -20,
+  },
+};
 
 const AccessPage = () => {
   const $accessPageMain = useRef<HTMLElement>(null);
@@ -32,6 +51,12 @@ const AccessPage = () => {
 
   return (
     <div className="blend-in-out">
+      <FloatingAstronaut
+        animationSpeed={AstronautStyleProps.animationSpeed}
+        maxWidth={AstronautStyleProps.maxWidth}
+        move={AstronautStyleProps.move}
+        rotation={AstronautStyleProps.rotation}
+      />
       <main ref={$accessPageMain} className="access-page-main text-hue-rotate">
         <span>Arthur Iturres</span>
         <h1>Delivering The Future</h1>
