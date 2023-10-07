d2fc84de3b5cafe642e48ed34aa6cd0b8df04fea
iturres <emanueliturres.co@gmail.com>
2023-10-06 23:13:57 -0300

    Feat: Add new Route to 'ProjectsPage' on 'HomePage' component

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -6,6 +6,7 @@ import LineCount from '../UI/LineCount.tsx';
 import Navbar from '../UI/Navbar.tsx';
 import ExpertiseSummaryPage from './ExpertiseSummaryPage.tsx';
 import ContactPage from './ContactPage.tsx';
+import ProjectsPage from './ProjectsPage.tsx';
 
 const HomePage: React.FC = () => (
   <>
@@ -16,6 +17,7 @@ const HomePage: React.FC = () => (
         <Route path="/" element={<AboutPage />} />
         <Route path="/expertise" element={<ExpertiseSummaryPage />} />
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
       </Route>
     </Routes>
   </>
