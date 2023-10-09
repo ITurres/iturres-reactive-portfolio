d83e2b7e46e4d10fa0eafb1ed535d13f931b41cb
iturres <emanueliturres.co@gmail.com>
2023-10-09 02:05:24 -0300

    Add new UI component 'SplideCarousel.tsx'

---
+++ src/components/UI/SplideCarousel.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import { Splide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+
+import SplideCarouselSlide from './SplideCarouselSlide.tsx';
+
+import getRandomId from '../../utils/getRandomId.ts';
+
+/* eslint-disable no-undef */
+interface SplideCarouselProps {
+  projectsGroup: Array<{}>;
+}
+
+const SplideCarousel: React.FC<SplideCarouselProps> = ({ projectsGroup }) => (
+  <>
+    <Splide
+      aria-label="Projects carousel"
+      options={{
+        rewind: true,
+        type: 'loop',
+        perPage: 3,
+        width: '100%',
+        height: '50%',
+        gap: '1rem',
+        cover: false,
+        isNavigation: true,
+        focus: 'center',
+        pagination: true,
+        mediaQuery: 'max',
+        breakpoints: {
+          800: {
+            arrows: false,
+            perPage: 1,
+          },
+          1048: {
+            perPage: 2,
+          },
+        },
+      }}
+    >
+      {projectsGroup.map((projectData) => (
+        <SplideCarouselSlide key={getRandomId()} projectData={projectData} />
+      ))}
+    </Splide>
+  </>
+);
+
+export default SplideCarousel;
