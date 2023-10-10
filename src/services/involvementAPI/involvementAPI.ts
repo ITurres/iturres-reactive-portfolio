df3a0a2cb93173d6fc991af0a1c76f0be06b1b2f
iturres <emanueliturres.co@gmail.com>
2023-10-10 01:46:19 -0300

    Add 'involvementAPI.ts' with 'postLike' and 'getLikes' async functions

---
+++ src/services/involvementAPI/involvementAPI.ts
@@ -0,0 +1,46 @@
+import config from '../../config.ts';
+
+const { baseURL, appId } = config.involvementAPI;
+
+const postLike = async (itemId: string) => {
+  try {
+    const response = await fetch(`${baseURL}${appId}/likes/`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        item_id: itemId,
+      }),
+      redirect: 'follow',
+    });
+    console.log(response);
+
+    if (!response.ok) return 'error';
+
+    const status = await response.text();
+    return status;
+  } catch (error) {
+    return error;
+  }
+};
+
+const getLikes = async () => {
+  try {
+    const response = await fetch(`${baseURL}${appId}/likes/`, {
+      method: 'GET',
+    });
+
+    if (!response.ok) return [];
+
+    const likes = await response.json();
+    return likes;
+  } catch (error) {
+    return error;
+  }
+};
+
+const involvement = {
+  postLike,
+  getLikes,
+};
+
+export default involvement;
