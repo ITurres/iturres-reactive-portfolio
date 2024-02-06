9b6668277cac10f13450d7cf5fcb5965d42e0d80
iturres <emanueliturres.co@gmail.com>
2024-02-06 19:58:33 -0300

    Feat: Update 'FileTabsNavbar' anchor elements to have active class
    - Store the url paths in an object and memoize them.
    - Use the 'useLocation' hook to get the current url path.
    - Conditional render the active class based on the current url path.

--- src/components/UI/FileTabsNavbar.tsx
+++ src/components/UI/FileTabsNavbar.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useMemo } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import { FaReact } from 'react-icons/fa';
 
@@ -8,11 +8,26 @@ import '../../styles/UI/FileTabsNavbar.scss';
 const FileTabsNavbar: React.FC = () => {
   const iconSize = 26;
 
+  const { pathname } = useLocation();
+
+  const paths = useMemo(
+    () => ({
+      homepage: '/iturres-reactive-portfolio/homepage',
+      projects: '/iturres-reactive-portfolio/homepage/projects',
+      contact: '/iturres-reactive-portfolio/homepage/contact',
+      expertise: '/iturres-reactive-portfolio/homepage/expertise',
+    }),
+    [],
+  );
+
   return (
     <nav className="file-tabs-navbar">
       <ul>
         <li>
-          <Link to="/iturres-reactive-portfolio/homepage">
+          <Link
+            to={paths.homepage}
+            className={pathname === paths.homepage ? 'active' : ''}
+          >
             <FaReact
               size={iconSize}
               className="tech-logo"
@@ -22,7 +37,10 @@ const FileTabsNavbar: React.FC = () => {
           </Link>
         </li>
         <li>
-          <Link to="/iturres-reactive-portfolio/homepage/projects">
+          <Link
+            to={paths.projects}
+            className={pathname === paths.projects ? 'active' : ''}
+          >
             <FaReact
               size={iconSize}
               className="tech-logo"
@@ -32,7 +50,10 @@ const FileTabsNavbar: React.FC = () => {
           </Link>
         </li>
         <li>
-          <Link to="/iturres-reactive-portfolio/homepage/contact">
+          <Link
+            to={paths.contact}
+            className={pathname === paths.contact ? 'active' : ''}
+          >
             <FaReact
               size={iconSize}
               className="tech-logo"
@@ -42,7 +63,10 @@ const FileTabsNavbar: React.FC = () => {
           </Link>
         </li>
         <li>
-          <Link to="/iturres-reactive-portfolio/homepage/expertise">
+          <Link
+            to={paths.expertise}
+            className={pathname === paths.expertise ? 'active' : ''}
+          >
             <FaReact
               size={iconSize}
               className="tech-logo"
