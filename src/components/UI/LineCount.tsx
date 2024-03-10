99deec9d247f640562f612a82cda1965a76012cf
ITurres <emanueliturres.co@gmail.com>
2024-03-10 00:01:49 -0300

    Fix: clean up event listener and add aria-hidden to true at 'LineCount.tsx'

--- src/components/UI/LineCount.tsx
+++ src/components/UI/LineCount.tsx
@@ -3,18 +3,25 @@ import React, { useEffect, useState } from 'react';
 const LineCount: React.FC = () => {
   const [windowHeight, setWindowHeight] = useState(window.innerHeight);
   const spanHeight = 24; // ? 24px.
-  const lines = Math.round(windowHeight / spanHeight);
+
   // ? rounds to ensure that 'line' is always an integer.
   // !important to prevent >>RangeError: Invalid array length<<
+  const lines = Math.round(windowHeight / spanHeight);
 
   useEffect(() => {
     window.addEventListener('resize', () => {
       setWindowHeight(window.innerHeight);
     });
+
+    return () => {
+      window.removeEventListener('resize', () => {
+        setWindowHeight(window.innerHeight);
+      });
+    };
   }, []);
 
   return (
-    <div className="line-count">
+    <div className="line-count" aria-hidden="true">
       {Array.from(Array(lines).keys()).map((line) => (
         <span key={line} className="line-count__line">
           {line + 1}
